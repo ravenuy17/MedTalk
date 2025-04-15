a800171d6a23817c7f3ccc68ec2893c5d2c3c760
MoodyBloo <angelodepasupil7@gmail.com>
2025-04-15 14:08:27 +0800

    register page modified 4/15/25

--- lib/pages/register_screen.dart
+++ lib/pages/register_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/components/my_button.dart';
+import 'package:flutter_application_1/components/my_textfield.dart';
+
+import '../components/square_tile.dart';
 
 class RegisterScreen extends StatefulWidget {
   final Function()? onTap;
@@ -9,8 +13,138 @@ class RegisterScreen extends StatefulWidget {
 }
 
 class _RegisterScreenState extends State<RegisterScreen> {
+
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final confirmPasswordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(height: 25),
+
+                const Icon(
+                  Icons.lock,
+                  size: 85,
+                ),
+
+                const SizedBox(height: 50),
+
+                Text(
+                  'Let\'s create an account for you!',
+                  style: TextStyle(
+                    color: Colors.grey[700],
+                    fontSize: 16,
+                  ),
+                ),
+
+                const SizedBox(height: 25),
+
+                MyTextfield(
+                  controller: emailController, 
+                  hintText: 'Email', 
+                  obscureText: false
+                ),
+
+                const SizedBox(height: 10),
+
+                MyTextfield(
+                  controller: passwordController, 
+                  hintText: 'password', 
+                  obscureText: true
+                ),
+
+                const SizedBox(height: 10),
+
+                MyTextfield(
+                  controller: confirmPasswordController, 
+                  hintText: 'Confirm Password', 
+                  obscureText: true
+                ),
+
+                const SizedBox(height: 10),
+
+                const SizedBox(height: 25),
+
+                MyButton(
+                  onTap: widget.onTap, 
+                  text: ('Sign Up'),
+                ),
+
+                const SizedBox(height: 50),
+
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Divider(
+                          thickness: 0.5,
+                          color: Colors.grey[400],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                        child: Text(
+                          'Or Continue with',
+                          style: TextStyle(color: Colors.grey[700]),
+                        ),
+                      ),
+                      Expanded(
+                        child: Divider(
+                          thickness: 0.5,
+                          color: Colors.grey[400],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+
+                const SizedBox(height: 50),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SquareTile(
+                      onTap: widget.onTap,
+                      imagePath: 'lib/assets/google.png'
+                    ),
+                  ],
+                ),
+
+                const SizedBox(height: 50),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Already have an account?',
+                      style: TextStyle(color: Colors.grey[700]),
+                    ),
+                    const SizedBox(width: 4),
+                    GestureDetector(
+                      onTap: widget.onTap,
+                      child: const Text(
+                        'Login Now',
+                        style: TextStyle(
+                          color: Colors.blue,
+                          fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
